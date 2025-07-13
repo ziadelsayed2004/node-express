9a7513efcec55dd63cc83611220646d9b26e78fe
ziadelsayed2004 <ziadelsayed2004@gmail.com>
2025-07-13 14:01:42 +0300

    new

--- controllers/coursesController.js
+++ controllers/coursesController.js
@@ -3,52 +3,58 @@ const courseModel = require("../models/coursesModel");
 const httpMsg = require("../utils/httpMsg");
 
 const getAllCourses = async (req, res) => {
-  const courses = await courseModel.getAll();
-  res.json({status:httpMsg.success, courses});
+  try {
+    const courses = await courseModel.getAll();
+    return res.json({status:httpMsg.success, courses});
+  } catch(err) {
+    return res.json({status:httpMsg.fail, errors: err.array()});
+  }
 };
 
 const getCourse = async (req, res) => {
   try{const course = await courseModel.getById(req.params.courseId);
-    if (!course) return res.status(404).json({status:httpMsg.notFound, msg: "Not Found" });
+    if (!course) return res.status(404).json({status:httpMsg.notFound, msg:"Not Found"});
     return res.json({status:httpMsg.success, course});
   } catch(err) {
-    return res.status(400).json(({status:httpMsg.notFound, msg: "ID Not Found" }));
+    return res.status(400).json(({status:httpMsg.notFound, msg:"ID Not Found", errors: err.array()}));
   }
 };
 
 const createCourse = async (req, res) => {
   const errors = validationResult(req);
   if (!errors.isEmpty())
-    return res.status(400).json({ errors: errors.array() });
-
+    return res.status(400).json({status:httpMsg.fail ,errors: errors.array()});
   try {
     const newCourse = await courseModel.create(req.body);
     res.status(201).json({status:httpMsg.success, newCourse});
   } catch (err) {
-    console.error("❌ Error creating course:", err);
-    res.status(500).json({status:httpMsg.fail,  msg: "Internal Server Error" });
+    res.status(500).json({status:httpMsg.fail, msg: "Internal Server Error", errors:err.array()});
   }
 };
 
 const updateCourse = async (req, res) => {
   const errors = validationResult(req);
-  if (!errors.isEmpty())
-    return res.status(400).json({ errors: errors.array() });
-
   const course = await courseModel.getById(req.params.courseId);
+  if (!errors.isEmpty()) return res.status(400).json({status:httpMsg.fail, errors: errors.array()});
   if (!course) return res.status(404).json({status:httpMsg.notFound, msg: "Not Found" });
-
-  await courseModel.update(req.params.courseId, req.body);
-  const updatedCourse = await courseModel.getById(req.params.courseId);
-  res.json({status:httpMsg.success, updatedCourse});
+  try{
+    await courseModel.update(req.params.courseId, req.body);
+    const updatedCourse = await courseModel.getById(req.params.courseId);
+    res.json({status:httpMsg.success, updatedCourse});
+  } catch(err){
+    res.status(500).json({status:httpMsg.fail, msg: "Internal Server Error", errors:err.array()});
+  }
 };
 
 const deleteCourse = async (req, res) => {
   const course = await courseModel.getById(req.params.courseId);
   if (!course) return res.status(404).json({status:httpMsg.notFound, msg: "Not Found" });
-
-  await courseModel.delete(req.params.courseId);
-  res.json({status:httpMsg.deleted,  msg: "Deleted", deleted: course });
+  try{
+    await courseModel.delete(req.params.courseId);
+    res.json({status:httpMsg.deleted,  msg: "Deleted", deleted: course });
+  } catch(err){
+    res.status(500).json({status:httpMsg.fail, msg: "Internal Server Error", errors:err.array()});
+  }
 };
 
 module.exports = {
