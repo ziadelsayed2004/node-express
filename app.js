0d80f0122a505d0186384f50b029e42c886a4f84
ziadelsayed2004 <ziadelsayed2004@gmail.com>
2025-07-18 22:40:06 +0300

    new

--- app.js
+++ app.js
@@ -33,11 +33,11 @@ app.use((err, req, res, next) => {
 });
 
 const { connectToMongo } = require('./config/db');
+connectToMongo();
+module.exports = app;
+
 connectToMongo().then(() => {
   app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
   });
 });
-
-connectToMongo();
-module.exports = app;
